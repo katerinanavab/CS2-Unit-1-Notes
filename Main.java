eae56a018fc192286a4b17a295d1c998df155784
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-09-10 13:24:11 +0000

    edit Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
+// Java programs are defined in a CLASS
+// the class name (Main) must match the FILE name (Main.java)
+// curly brackets define the start & end of a section
 public class Main {
 
+   // The main method is what runs when you press RUN
    public static void main(String []args) {
+      // Below is a print STATEMENT (instruction/command)
+      // Java statements end in a semicolon
       System.out.println("Hello World");
+      
+      // print will output on the same line
+      // println will enter to the next line after printing
+      System.out.print("apcs");
+      System.out.print("is fun");
+
+
+
    }
 }
